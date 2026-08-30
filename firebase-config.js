d379ed13353f120d0868dd3f18696f2bbbbc2735
mdjawedqamar65 <mdjawedqamar65@gmail.com>
2026-08-30 16:01:21 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,17 +1,15 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.10.0/firebase-firestore.js";
 
-// Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDTCec6s4g50jxDFcp5InKaIWojZrjJgc",
+  apiKey: "AIzaSyDTCEc6s4g5OjxDfFCp5TnKaIWOjZrjEgc",
   authDomain: "sohana-beauty-saloon.firebaseapp.com",
   projectId: "sohana-beauty-saloon",
   storageBucket: "sohana-beauty-saloon.firebasestorage.app",
   messagingSenderId: "454248308554",
-  appId: "1:454248308554:web:51044f7cf825824ffcebb3"
+  appId: "1:454248308554:web:767d53c58fe2073ffcebb3"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
